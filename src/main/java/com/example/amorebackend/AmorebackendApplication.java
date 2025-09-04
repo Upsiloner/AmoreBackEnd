69d79d2ee34a92a763e231d104570dabd44384f2
Upsiloner <1458335652@qq.com>
2025-09-04 22:58:25 +0800

    chore and test: initial constraction, helloworld test

---
+++ src/main/java/com/example/amorebackend/AmorebackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.amorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmorebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmorebackendApplication.class, args);
+	}
+}
